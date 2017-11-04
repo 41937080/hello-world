385bae557ca7c2d05298fa334251bde97ca9a8f2
41937080 <41937080@qq.com>
2017-11-04 14:25:20 +0800

    save

---
+++ everest/farmus/app/common/app_common.h
@@ -0,0 +1,18 @@
+/*
+
+*/
+
+#ifndef APP_COMMON_H
+#define APP_COMMON_H
+
+
+/* Generic return valud*/
+typedef enum
+{
+  return_st_success = 0,
+  return_st_error,
+  return_st_timeout,
+} return_st_t;
+
+
+
