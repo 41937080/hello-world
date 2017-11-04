eb56c99de8a3023307bc4b3a33cee75210078163
41937080 <41937080@qq.com>
2017-11-04 14:21:37 +0800

    save

--- everest/farmus/app/src/services/dma_main_service.c
+++ everest/farmus/app/src/services/dma_main_service.c
@@ -1,6 +1,12 @@
 /*
-
-
+main()
+|
+Dma_Main_service_init() : create task DmaMainService();
+|
+vTaskDmaMainService() : step1 : dma interrupt init, Dma_Main_INT_init();
+                        step2 : dma register init,  Dma_Main_CFG_init();
+                        step3 : for(;;), do what you like, e.g. print;
+ 
 */
 
 #include <semphr.h>
@@ -23,8 +29,13 @@ static dma_irq_handler_inst_t g_inst[1] =
 void dma_irqhandler_st(void* userdata)
 {
   dma_irq_handler_inst_t* inst = (dma_irq_handler_inst_t*) userdata;
-  inst->irqStatusLast = //read irq status register;
-  //clear irq
+  //read irq status to clear irq
+  inst->irqStatusLast = Xil_In32(0xa0000018);
+  xSemaphoreGiveFromISR(inst->irqBinaryMutex,NULL);
+  //TODO
+  //BaseType_t xHiPrioTaskWoken = 0;
+  //xSemaphoreGiveFromISR(inst->irqBinaryMutex,&xHiPrioTaskWoken);
+  //portYIELD_FROM_ISR(xHiPrioTaskWoken);
 }
 
 
