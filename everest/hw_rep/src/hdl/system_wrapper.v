d13fb551d3eed5d32bdc561b30365aedd25ad8ba
41937080 <41937080@qq.com>
2017-12-16 14:18:07 +0800

    add hw_rep & sw_rep

---
+++ everest/hw_rep/src/hdl/system_wrapper.v
@@ -0,0 +1,18 @@
+//Copyright 1986-2017 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2017.2 (win64) Build 1909853 Thu Jun 15 18:39:09 MDT 2017
+//Date        : Sat Dec 16 13:45:12 2017
+//Host        : MQA7BMPV78PR48Y running 64-bit Service Pack 1  (build 7601)
+//Command     : generate_target system_wrapper.bd
+//Design      : system_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module system_wrapper
+   ();
+
+
+  system system_i
+       ();
+endmodule
